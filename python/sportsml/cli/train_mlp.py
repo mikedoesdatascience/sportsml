e82ce27143cf5141a4bc3c352ad58bbff1eeb5b1
MikeDoesDataScience <mike@mikedoesdatascience.dev>
2023-02-27 02:13:56 +0000

    add seeding

--- python/sportsml/cli/train_mlp.py
+++ python/sportsml/cli/train_mlp.py
@@ -1,8 +1,11 @@
 import hydra
+import pytorch_lightning as pl
 from omegaconf import DictConfig, OmegaConf
 
 @hydra.main(version_base=None, config_path="conf", config_name="mlp")
 def train(cfg : DictConfig) -> None:
+    if cfg.seed is not None:
+        pl.seed_everything(cfg.seed)
     trainer = hydra.utils.instantiate(cfg.trainer)
     model = hydra.utils.instantiate(cfg.model)
     dm = hydra.utils.instantiate(cfg.dm)
