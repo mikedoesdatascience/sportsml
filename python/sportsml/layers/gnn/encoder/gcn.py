93ecb6391c9e5ce52b5d891f2c8f07360946e530
mike <mike@mikedoesdatascience.dev>
2023-03-19 15:56:02 +0000

    refactor gcn encoder

--- python/sportsml/layers/gnn/encoder/gcn.py
+++ python/sportsml/layers/gnn/encoder/gcn.py
@@ -2,16 +2,34 @@ import dgl
 import torch
 
 class GCNEncoder(torch.nn.Module):
-    def __init__(self, in_feats, out_feats=100, depth=3):
+    def __init__(self, in_feats, out_feats=100, depth=3, dropout=0.1):
         super().__init__()
         self.depth = depth
-        self.norm = torch.nn.BatchNorm1d(in_feats)
-        self.w_i = torch.nn.Linear(in_feats, out_feats)
-        self.w_h = torch.nn.Linear(3 * out_feats + 1, out_feats)
+        self.layer_init = torch.nn.Sequential(
+            torch.nn.BatchNorm1d(in_feats),
+            torch.nn.Linear(in_feats, out_feats),
+            torch.nn.ReLU(),
+            torch.nn.Dropout(dropout)
+        )
+        self.layers = torch.nn.ModuleList([
+            torch.nn.Sequential(
+                torch.nn.BatchNorm1d(3 * out_feats + 1),
+                torch.nn.Linear(3 * out_feats + 1, out_feats),
+                torch.nn.ReLU(),
+                torch.nn.Dropout(dropout)
+            )
+            for _ in range(depth)
+        ])
+        self.layer_out = torch.nn.Sequential(
+            torch.nn.BatchNorm1d(2* out_feats),
+            torch.nn.Linear(2* out_feats, out_feats),
+            torch.nn.ReLU(),
+            torch.nn.Dropout(dropout)
+        )
     
     def forward(self, g, e):
         g = g.local_var()
-        g.edata['f'] = torch.nn.functional.relu(self.w_i(self.norm(e)))
+        g.edata['f'] = self.layer_init(e)
         g.update_all(
             dgl.function.copy_e('f', 'm'),
             dgl.function.reducer.mean('m', 'f')
@@ -20,12 +38,12 @@ class GCNEncoder(torch.nn.Module):
         g.ndata['h'] = g.ndata['f']
         
         src, dst = g.edges()
-        for _ in range(self.depth):
+        for layer in self.layers:
             x = torch.cat([g.ndata['h'][src], g.edata['f'], g.edata['p'], g.ndata['h'][dst]], dim=1)
-            x = torch.nn.functional.relu(self.w_h(x))
+            x = layer(x)
             g.edata['h'] = x
             g.update_all(
                 dgl.function.copy_e('h', 'm'),
                 dgl.function.reducer.mean('m', 'h')
             )
-        return torch.cat([g.ndata['f'], g.ndata['h']], dim=1)
+        return self.layer_out(torch.cat([g.ndata['f'], g.ndata['h']], dim=1))
