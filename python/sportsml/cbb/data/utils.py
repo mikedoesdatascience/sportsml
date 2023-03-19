b961505944fe3b797e655d4fde1749aa644d7ea6
mike <mike@mikedoesdatascience.dev>
2023-03-19 22:39:37 +0000

    get latest graph

--- python/sportsml/cbb/data/utils.py
+++ python/sportsml/cbb/data/utils.py
@@ -1,8 +1,16 @@
 import pandas as pd
 
+from .datamodule import CBBGraphDataset
 from ...mongo import client
 
 
 def get_games(query={}):
     df = pd.DataFrame(client.cbb.games.find(query)).sort_values(['Season', 'DayNum'])
     return df
+
+
+def get_latest_graph():
+    games = get_games({'Season': max(client.cbb.games.distinct('Season'))})
+    ds = CBBGraphDataset(games)
+    return ds[-1]
+
