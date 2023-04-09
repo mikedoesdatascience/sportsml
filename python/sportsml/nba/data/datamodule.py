7515c9a0fe4513a23abfc37ab3d666d7a55f20cf
MikeDoesDataScience <mike@mikedoesdatascience.dev>
2023-04-09 17:52:50 +0000

    enable multitask

--- python/sportsml/nba/data/datamodule.py
+++ python/sportsml/nba/data/datamodule.py
@@ -14,7 +14,7 @@ class NBAGameDataModule(pl.LightningDataModule):
         val_df=None,
         test_df=None,
         feature_columns=FEATURE_COLUMNS,
-        target_column="PLUS_MINUS",
+        target_columns=["PLUS_MINUS"],
         batch_size=64,
         splits=[0.8, 0.1, 0.1],
         num_workers=4,
@@ -24,14 +24,14 @@ class NBAGameDataModule(pl.LightningDataModule):
         self.val_df = val_df
         self.test_df = test_df
         self.feature_columns = feature_columns
-        self.target_column = target_column
+        self.target_columns = target_columns
         self.batch_size = batch_size
         self.splits = splits
         self.num_workers = num_workers
 
     def df_to_dataset(self, df):
         X = df[self.feature_columns].values
-        y = df[[self.target_column]].values
+        y = df[self.target_columns].values
         return torch.utils.data.TensorDataset(
             torch.from_numpy(X).float(), torch.from_numpy(y).float()
         )
@@ -73,10 +73,12 @@ class NBAGameDataModule(pl.LightningDataModule):
 
 
 class NBAGraphDataset(object):
-    def __init__(self, df, feature_columns=GRAPH_FEATURES, target_column="PLUS_MINUS"):
+    def __init__(
+        self, df, feature_columns=GRAPH_FEATURES, target_columns=["PLUS_MINUS"]
+    ):
         self.df = df
         self.feature_columns = feature_columns
-        self.target_column = target_column
+        self.target_columns = target_columns
         seasons = df["SEASON"].unique()
         self.dates = []
         for season in seasons:
@@ -119,7 +121,7 @@ class NBAGraphDataset(object):
             num_nodes=30,
         )
         g.edata["f"] = torch.from_numpy(self.df[self.feature_columns].values).float()
-        g.edata["y"] = torch.from_numpy(self.df[[self.target_column]].values).float()
+        g.edata["y"] = torch.from_numpy(self.df[self.target_columns].values).float()
         g.edata["p"] = torch.from_numpy(self.df[["HOME"]].values).float()
         g.edata["rest"] = torch.from_numpy(self.df[["REST"]].values).float()
         g.edata["date"] = torch.from_numpy(
@@ -134,7 +136,7 @@ class NBAGraphDataModule(pl.LightningDataModule):
         self,
         df,
         feature_columns=GRAPH_FEATURES,
-        target_column="PLUS_MINUS",
+        target_columns=["PLUS_MINUS"],
         batch_size=64,
         split_type="random",
         splits=[0.8, 0.1, 0.1],
@@ -143,14 +145,14 @@ class NBAGraphDataModule(pl.LightningDataModule):
         super().__init__()
         self.df = df
         self.feature_columns = feature_columns
-        self.target_column = target_column
+        self.target_columns = target_columns
         self.batch_size = batch_size
         self.split_type = split_type
         self.splits = splits
         self.num_workers = num_workers
 
     def setup(self, stage="train"):
-        self.ds = NBAGraphDataset(self.df, self.feature_columns, self.target_column)
+        self.ds = NBAGraphDataset(self.df, self.feature_columns, self.target_columns)
         if self.split_type == "random":
             self.train_ds, self.val_ds, self.test_ds = torch.utils.data.random_split(
                 self.ds, self.splits
