17fc94940fb8e950b6a6dbccfa4ce7131f35244c
MikeDoesDataScience <mike@mikedoesdatascience.dev>
2024-03-10 15:42:00 +0000

    use boolean home_mask

--- python/sportsml/utils/heterograph.py
+++ python/sportsml/utils/heterograph.py
@@ -23,17 +23,23 @@ def heterograph_predictor(src, dst, feat, home_mask, num_nodes, y=None):
     graph = dgl.heterograph(
         {
             ("team", "home", "team"): (
-                (torch.tensor(src[home_mask]), torch.tensor(dst[home_mask]))
+                (
+                    torch.tensor(src[home_mask.astype(bool)]),
+                    torch.tensor(dst[home_mask.astype(bool)]),
+                )
             ),
             ("team", "away", "team"): (
-                (torch.tensor(src[~home_mask]), torch.tensor(dst[~home_mask]))
+                (
+                    torch.tensor(src[~home_mask.astype(bool)]),
+                    torch.tensor(dst[~home_mask.astype(bool)]),
+                )
             ),
         },
         {"team": num_nodes},
     )
-    graph.edges["home"].data["f"] = torch.tensor(feat[home_mask]).float()
-    graph.edges["away"].data["f"] = torch.tensor(feat[~home_mask]).float()
+    graph.edges["home"].data["f"] = torch.tensor(feat[home_mask.astype(bool)]).float()
+    graph.edges["away"].data["f"] = torch.tensor(feat[~home_mask.astype(bool)]).float()
     if y is not None:
-        graph.edges["home"].data["y"] = torch.tensor(y[home_mask]).float()
-        graph.edges["away"].data["y"] = torch.tensor(y[~home_mask]).float()
+        graph.edges["home"].data["y"] = torch.tensor(y[home_mask.astype(bool)]).float()
+        graph.edges["away"].data["y"] = torch.tensor(y[~home_mask.astype(bool)]).float()
     return graph
