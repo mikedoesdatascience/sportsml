5bb86987860b34bf663a31d4659d28c5043a3d5b
mikedoesdatascience <147824788+mikedoesdatascience@users.noreply.github.com>
2025-12-31 18:40:34 +0000

    expose version command

--- src/sportsml/cli.py
+++ src/sportsml/cli.py
@@ -1,8 +1,14 @@
 import jsonargparse
 
+from . import __version__
 from .cfb.data.download import download as cfb_download
 from .nba.data.download import download as nba_download
 from .nfl.data.download import download as nfl_download
+
+
+def version():
+    """Print the version of the sportsml package."""
+    print(__version__)
 
 
 def cli():
@@ -11,6 +17,7 @@ def cli():
             "cfb": {"download": cfb_download},
             "nba": {"download": nba_download},
             "nfl": {"download": nfl_download},
+            "version": version,
         },
         as_positional=False,
     )
