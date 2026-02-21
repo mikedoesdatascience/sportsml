13d8902580bfe60222e68203fa070165ffcffdb3
Mike <147824788+mikedoesdatascience@users.noreply.github.com>
2026-02-21 16:55:42 +0000

    add rf train

--- src/sportsml/cli.py
+++ src/sportsml/cli.py
@@ -4,8 +4,11 @@ import pandas as pd
 
 from . import __version__
 from .cbb.data.download import download as cbb_download
+from .cbb.data.features import CATEGORICAL_COLUMNS as CBB_CATEGORICAL_COLUMNS
 from .cbb.data.features import STATS_COLUMNS as CBB_STATS_COLUMNS
 from .cfb.data.download import download as cfb_download
+from .cfb.data.features import CATEGORICAL_COLUMNS as CFB_CATEGORICAL_COLUMNS
+from .cfb.data.features import STATS_COLUMNS as CFB_STATS_COLUMNS
 from .models.rf import train_rf
 from .nba.data.download import download as nba_download
 from .nfl.data.download import download as nfl_download
@@ -27,15 +30,17 @@ def cli():
     jsonargparse.auto_cli(
         {
             "cbb": {"download": cbb_download, "rf": {"train": train_rf}},
-            "cfb": {"download": cfb_download},
-            "nba": {"download": nba_download},
+            "cfb": {"download": cfb_download, "rf": {"train": train_rf}},
+            "nba": {"download": nba_download, "rf": {"train": train_rf}},
             "nfl": {"download": nfl_download, "rf": {"train": train_rf}},
             "version": version,
         },
         as_positional=False,
         set_defaults={
             "cbb.download.output_file": "data/cbb/raw.csv",
+            "cbb.rf.train.games": "data/cbb/raw.csv",
             "cbb.rf.train.stats_columns": CBB_STATS_COLUMNS,
+            "cbb.rf.train.categorical_columns": CBB_CATEGORICAL_COLUMNS,
             "cbb.rf.train.target_column": "PlusMinus",
             "cbb.rf.train.season_column": "Season",
             "cbb.rf.train.date_column": "DayNum",
@@ -43,8 +48,18 @@ def cli():
             "cbb.rf.train.team_opp_column": "TeamID_OPP",
             "cbb.rf.train.print_metrics": True,
             "cfb.download.output_file": "data/cfb/raw.csv",
+            "cfb.rf.train.games": "data/cfb/raw.csv",
+            "cfb.rf.train.stats_columns": CFB_STATS_COLUMNS,
+            "cfb.rf.train.categorical_columns": CFB_CATEGORICAL_COLUMNS,
+            "cfb.rf.train.target_column": "result",
+            "cfb.rf.train.season_column": "season",
+            "cfb.rf.train.date_column": "week",
+            "cfb.rf.train.team_column": "team",
+            "cfb.rf.train.team_opp_column": "opp_team",
+            "cfb.rf.train.print_metrics": True,
             "nba.download.output_file": "data/nba/raw.csv",
             "nfl.download.output_file": "data/nfl/raw.csv",
+            "nfl.rf.train.games": "data/nfl/raw.csv",
             "nfl.rf.train.stats_columns": NFL_STATS_COLUMNS,
             "nfl.rf.train.meta_columns": NFL_META_COLUMNS,
             "nfl.rf.train.categorical_columns": NFL_CATEGORICAL_COLUMNS,
