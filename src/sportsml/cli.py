3735f9ff85fd7982ddae0b8ec17e2f7cc63c93ab
Mike <147824788+mikedoesdatascience@users.noreply.github.com>
2026-02-21 16:32:39 +0000

    add nfl rf train

--- src/sportsml/cli.py
+++ src/sportsml/cli.py
@@ -9,6 +9,9 @@ from .cfb.data.download import download as cfb_download
 from .models.rf import train_rf
 from .nba.data.download import download as nba_download
 from .nfl.data.download import download as nfl_download
+from .nfl.data.features import CATEGORICAL_COLUMNS as NFL_CATEGORICAL_COLUMNS
+from .nfl.data.features import META_COLUMNS as NFL_META_COLUMNS
+from .nfl.data.features import STATS_COLUMNS as NFL_STATS_COLUMNS
 
 jsonargparse.typing.register_type(
     pd.DataFrame, lambda x: getattr(x, "filename"), lambda x: pd.read_csv(x)
@@ -26,7 +29,7 @@ def cli():
             "cbb": {"download": cbb_download, "rf": {"train": train_rf}},
             "cfb": {"download": cfb_download},
             "nba": {"download": nba_download},
-            "nfl": {"download": nfl_download},
+            "nfl": {"download": nfl_download, "rf": {"train": train_rf}},
             "version": version,
         },
         as_positional=False,
@@ -42,6 +45,15 @@ def cli():
             "cfb.download.output_file": "data/cfb/raw.csv",
             "nba.download.output_file": "data/nba/raw.csv",
             "nfl.download.output_file": "data/nfl/raw.csv",
+            "nfl.rf.train.stats_columns": NFL_STATS_COLUMNS,
+            "nfl.rf.train.meta_columns": NFL_META_COLUMNS,
+            "nfl.rf.train.categorical_columns": NFL_CATEGORICAL_COLUMNS,
+            "nfl.rf.train.target_column": "result",
+            "nfl.rf.train.season_column": "season",
+            "nfl.rf.train.date_column": "week",
+            "nfl.rf.train.team_column": "team",
+            "nfl.rf.train.team_opp_column": "opp_team",
+            "nfl.rf.train.print_metrics": True,
         },
         parser_mode="omegaconf+",
     )
