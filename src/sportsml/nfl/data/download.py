72872b497e208e2ab779497ea713065e70cb7022
Mike <147824788+mikedoesdatascience@users.noreply.github.com>
2026-03-07 13:27:02 +0000

    update nfl download to use nflreadpy

--- src/sportsml/nfl/data/download.py
+++ src/sportsml/nfl/data/download.py
@@ -1,48 +1,91 @@
 from pathlib import Path
 
+import nflreadpy
 import pandas as pd
 
+from .features import STATS_COLUMNS
+from .names import move_map
 from .nodes import team_abr_lookup
-from .utils import merge_games_schedule
-
-DATA_URL = (
-    "https://github.com/nflverse/nflverse-data/releases/download/player_stats/"
-    "player_stats.parquet"
-)
 
 
-def get_play_by_play():
-    data = pd.read_parquet(DATA_URL)
-    return data
+def invert_schedule(schedule: pd.DataFrame):
+    home = schedule.copy()
+    away = schedule.copy()
 
+    away["result"] *= -1
+    away["spread_line"] *= -1
+    away["location"] = away["location"].replace("Home", "Away")
 
-def get_game_totals():
-    data = get_play_by_play()
-    game_totals = (
-        data.groupby(["recent_team", "season", "week"])
-        .sum(numeric_only=True)
-        .reset_index()
+    home_col_renamer = {
+        col: col.removeprefix("home_") for col in home if col.startswith("home_")
+    }
+    home_col_renamer.update(
+        {col: col.replace("away_", "opp_") for col in home if col.startswith("away_")}
     )
-    return game_totals
+    home = home.rename(columns=home_col_renamer)
 
-
-def get_schedule():
-    schedule = pd.pandas.read_csv(
-        "https://github.com/nflverse/nfldata/raw/master/data/games.csv"
+    away_col_renamer = {
+        col: col.removeprefix("away_") for col in away if col.startswith("away_")
+    }
+    away_col_renamer.update(
+        {col: col.replace("home_", "opp_") for col in away if col.startswith("home_")}
     )
-    return schedule
+    away = away.rename(columns=away_col_renamer)
+
+    return pd.concat([home, away])
 
 
 def download(output_file: str = None):
-    games = get_game_totals()
-    schedule = get_schedule()
-    games = merge_games_schedule(games, schedule)
-    games["game_id"] = games["_id"].apply(
-        lambda x: "-".join(x.split("-")[:2] + sorted(x.split("-")[-2:]))
+    team_stats = (
+        nflreadpy.load_team_stats(seasons=True)
+        .to_pandas()
+        .rename(columns={"opponent_team": "opp_team"})
     )
+
+    team_stats["team"] = team_stats["team"].replace(move_map)
+    team_stats["opp_team"] = team_stats["opp_team"].replace(move_map)
+
+    schedule = nflreadpy.load_schedules(True).to_pandas()
+    schedule = invert_schedule(schedule=schedule).drop(columns=["game_id"])
+
+    schedule["team"] = schedule["team"].replace(move_map)
+    schedule["opp_team"] = schedule["opp_team"].replace(move_map)
+
+    games = team_stats.merge(
+        schedule, on=["season", "week", "team", "opp_team"], how="outer"
+    )
+
+    games['game_id'] = games[['game_id', 'old_game_id']].bfill(axis=1)["game_id"]
+
+    games = games.dropna(subset=["game_id"])
+
+    first_game = games.drop_duplicates(subset=["game_id"], keep="first")
+    last_game = games.drop_duplicates(subset=["game_id"], keep="last")
+
+    games = pd.concat(
+        [
+            first_game.merge(
+                last_game[STATS_COLUMNS + ["game_id"]]
+                .add_prefix("opp_")
+                .rename(columns={"opp_game_id": "game_id"}),
+                on="game_id",
+                how="left",
+            ).set_index(first_game.index),
+            last_game.merge(
+                first_game[STATS_COLUMNS + ["game_id"]]
+                .add_prefix("opp_")
+                .rename(columns={"opp_game_id": "game_id"}),
+                on="game_id",
+                how="left",
+            ).set_index(last_game.index),
+        ]
+    )
+
     games["src"] = games["opp_team"].map(team_abr_lookup)
     games["dst"] = games["team"].map(team_abr_lookup)
+
     if output_file is not None:
         Path(output_file).parent.mkdir(parents=True, exist_ok=True)
         games.to_csv(output_file, index=False)
+
     return games
